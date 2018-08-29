894be756d527376c7d69a32ecea5a12bb63c60ca
williatsanders <williamtsanders@gmail.com>
2018-08-29 11:35:29 -0500

    added CommentForm component

---
+++ client/src/CommentForm.js
@@ -0,0 +1,28 @@
+// CommentForm.js
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const CommentForm = props => (
+  <form onSubmit={props.submitComment}>
+    <input
+      type="text"
+      name="author"
+      placeholder="Your name..."
+      value={props.text}
+      onChange={props.handleTextChange}
+    />
+    <button type="submit">Submit</button>
+  </form>
+);
+
+CommentForm.propTypes = {
+  submitComment: PropTypes.func.isRequired,
+  handleChangeText: PropTypes.func.isRequired,
+  text: PropTypes.string,
+  author: PropTypes.string,
+};
+
+CommentForm.defaultProps = {
+  text: '',
+  author: '',
+};
